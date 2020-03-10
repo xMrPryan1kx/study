8f782c366b1fde1816ca151fd13f5b1692eb9846
xMrPryan1kx <alekseythxorev@gmail.com>
2020-03-10 07:44:38 +0300

    up

--- Project/Landings/MySite/scrypt.js
+++ Project/Landings/MySite/scrypt.js
@@ -1,45 +1,32 @@
-const containerUl = document.querySelector('.nav-bar-list')
-let main = document.querySelectorAll("main div");
+const header = document.querySelector(".nav-bar-list");
+const tab = document.querySelectorAll(".nav-bar-list li");
+const content = document.querySelectorAll(".main div");
 
 
+function hideTabContent() {
+  content.forEach(item => {
+    item.style.display = 'none';
+  });
 
-
-function toggleActive(name) {
-    const li = document.querySelectorAll("li");
-
-    Array.from(li).forEach(item => {
-        item.classList.remove('active')
-    })
-  name.classList.add("active");
-  
-}
-
-
-
-
-
-// li[0].addEventListener("click", function() {
-//   toggleActive(main[0]);
-// });
-// li[1].addEventListener("click", function() {
-//   toggleActive(main[1]);
-// });
-// li[2].addEventListener("click", function() {
-//   toggleActive(main[2]);
-// });
-// li[3].addEventListener("click", function() {
-//   toggleActive(main[3]);
-// });
-
-function all(touch, render) {
-  touch.addEventListener("click", function() {
-    toggleActive(render);
+  tab.forEach(item => {
+    item.classList.remove('active')
   });
 }
 
-all(li[0], main[0])
-all(li[1], main[1])
-all(li[2], main[2])
-all(li[3], main[3])
+function showTabContent(i = 0) {
+  content[i].style.display = 'block';
+  tab[i].classList.add('active');
+}
+hideTabContent();
+showTabContent();
 
 
+header.addEventListener('click', (e) => {
+  const target = e.target;
+  tab.forEach((item, i) => {
+    if (target == item || target.parentNode == item) {
+      hideTabContent();
+      showTabContent(i);
+    }
+  })
+})
